4ff3105974c080f546389d266e3e84cfa526d840
Maxim Kolesov <pro100maks222@gmail.com>
2024-03-02 12:50:07 +0300

    Initial commit

---
+++ src/test/java/kolesov/maksim/mapping/map/MapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kolesov.maksim.mapping.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
